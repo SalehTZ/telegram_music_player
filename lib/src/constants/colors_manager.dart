cce4c1b4077e28b6b5ee4da718de84f8a546800e
SalehTZ <31685655+SalehTZ@users.noreply.github.com>
2023-12-28 20:09:01 +0330

    Added needed managers.

---
+++ lib/src/constants/colors_manager.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  //! New Color Scheme
+  static const Color error = Colors.red; // TODO change error color later
+  static const Color scaffoldBgColor = Color(0xFFF5F5F5);
+  static const Color likeButtonColor = Color(0xFFFE3B7C);
+  static const Color splashScreenBgColor = Color(0xFF6C5FBC);
+  static const Color bottomSheetHandleColor = Color(0xFFEEEEEE);
+  static const Color customTruckStatusOpenColor = Color(0xFF3EA400);
+  static const Color customTruckStatusCloseColor = Color(0xFFFE3A7C);
+
+  //* Primary Colors
+  static Color primaryMaterial = HexColor.toMaterialColor(0xFF5947B2);
+  static const Color primaryEnabledColor = Color(0xFF5947B2);
+  static const Color primaryPressedColor = Color(0xFF7A6CC1);
+  static const Color primaryDisabledColor = Color(0xFFACA3D8);
+
+  // * Shadow Colors
+  static const Color hardDropShadowColor = Color(0x26000000);
+  static const Color softDropShadowColor = Color(0x0A000000);
+}
+
+// * An extension to convert Hex to Color class
+extension HexColor on Color {
+  /// Convert any Hex color to Material Color.
+  /// Input: color without alpha example #FF94C7
+  static MaterialColor toMaterialColor(int hexColorString) {
+    final color = Color(hexColorString);
+    final int red = color.red;
+    final int green = color.green;
+    final int blue = color.blue;
+
+    final Map<int, Color> shades = {
+      50: Color.fromRGBO(red, green, blue, .1),
+      100: Color.fromRGBO(red, green, blue, .2),
+      200: Color.fromRGBO(red, green, blue, .3),
+      300: Color.fromRGBO(red, green, blue, .4),
+      400: Color.fromRGBO(red, green, blue, .5),
+      500: Color.fromRGBO(red, green, blue, .6),
+      600: Color.fromRGBO(red, green, blue, .7),
+      700: Color.fromRGBO(red, green, blue, .8),
+      800: Color.fromRGBO(red, green, blue, .9),
+      900: Color.fromRGBO(red, green, blue, 1),
+    };
+
+    return MaterialColor(color.value, shades);
+  }
+}
---
+++ lib/src/constants/values_manager.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class AppMargin {
+  static const double m8 = 8.0;
+  static const double m12 = 12.0;
+  static const double m14 = 14.0;
+  static const double m16 = 16.0;
+  static const double m18 = 18.0;
+  static const double m20 = 20.0;
+}
+
+class AppPadding {
+  static const double p1 = 1.0;
+  static const double p2 = 2.0;
+  static const double p4 = 4.0;
+  static const double p8 = 8.0;
+  static const double p12 = 12.0;
+  static const double p14 = 14.0;
+  static const double p16 = 16.0;
+  static const double p18 = 18.0;
+  static const double p20 = 20.0;
+  static const double p24 = 24.0;
+  static const double p30 = 30.0;
+}
+
+class AppSize {
+  static const double s0 = 0;
+  static const double s4 = 4.0;
+  static const double s8 = 8.0;
+  static const double s10 = 10.0;
+  static const double s12 = 12.0;
+  static const double s14 = 14.0;
+  static const double s16 = 16.0;
+  static const double s18 = 18.0;
+  static const double s20 = 20.0;
+  static const double s24 = 24.0;
+  static const double s28 = 28.0;
+  static const double s30 = 30.0;
+  static const double s32 = 32.0;
+  static const double s36 = 36.0;
+  static const double s40 = 40.0;
+  static const double s48 = 48.0;
+  static const double s50 = 50.0;
+  static const double s56 = 56.0;
+  static const double s60 = 60.0;
+  static const double s64 = 64.0;
+  static const double s80 = 80.0;
+  static const double s84 = 84.0;
+  static const double s86 = 86.0;
+  static const double s90 = 90.0;
+  static const double s162 = 162.0;
+}
+
+class AppRadius {
+  static const rad8 = Radius.circular(8);
+  static const rad12 = Radius.circular(12);
+  static const rad16 = Radius.circular(16);
+  static const rad999 = Radius.circular(999);
+}
+
+class AppDuration {
+  // for milliseconds
+  static const int dms300 = 300;
+  static const int dms500 = 500;
+  static const int dms800 = 800;
+
+  // for seconds
+  static const int ds1 = 1;
+  static const int ds5 = 5;
+  static const int ds8 = 8;
+
+  // for minutes
+  static const int dm1 = 1;
+  static const int dm5 = 5;
+  static const int dm8 = 8;
+}
