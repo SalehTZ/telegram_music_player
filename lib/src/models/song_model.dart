4959ec091eeaf0bd0bef63716eae373b1c5b283c
SalehTZ <31685655+SalehTZ@users.noreply.github.com>
2023-12-31 16:35:13 +0330

    Created song model.

---
+++ lib/src/models/song_model.dart
@@ -0,0 +1,23 @@
+class Song {
+  final String title;
+  final String artist;
+  final String album;
+  final String filePath; // Or a URL if streaming from the internet
+  final Duration duration;
+  final String? albumArtUrl; // URL to the album art image
+  final int? rating; // Rating out of 5
+  final String? genre;
+  final int? releaseDate;
+
+  Song({
+    required this.title,
+    required this.artist,
+    required this.album,
+    required this.filePath,
+    required this.duration,
+    this.albumArtUrl,
+    this.rating,
+    this.genre,
+    this.releaseDate,
+  });
+}
