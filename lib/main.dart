8e08c878fe2cefc1e24f05c10b81779eac55732f
SalehTZ <31685655+SalehTZ@users.noreply.github.com>
2023-12-31 16:36:43 +0330

    Added bloc observer to observe bloc instances.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'src/app.dart';
 
 void main() {
+  Bloc.observer = SimpleBlocObserver();
   runApp(const MusicPlayerApp());
+}
+
+/// Custom [BlocObserver] which observes all bloc and cubit instances.
+class SimpleBlocObserver extends BlocObserver {
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    super.onEvent(bloc, event);
+    debugPrint(event.toString());
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    debugPrint(transition.toString());
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    debugPrint(error.toString());
+    super.onError(bloc, error, stackTrace);
+  }
 }
