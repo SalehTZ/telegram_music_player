c4b01cd94b13593e81eba510467696609730a74d
SalehTZ <31685655+SalehTZ@users.noreply.github.com>
2024-01-10 18:03:21 +0330

    Created temporary mock artist data.

---
+++ lib/src/data/fake_artist_data.dart
@@ -0,0 +1,16 @@
+import 'package:telegram_music_player/src/models/artist_model.dart';
+
+var fakeArtistsList = [
+  Artist(
+    name: "Billie Eilish",
+  ),
+  Artist(
+    name: "Arctic Monkeys",
+  ),
+  Artist(
+    name: "Weeknd",
+  ),
+  Artist(
+    name: "Daft Punk",
+  ),
+];
