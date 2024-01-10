5fb9202da4c8b75db535fe04c20b3150b0106ca0
SalehTZ <31685655+SalehTZ@users.noreply.github.com>
2024-01-10 18:03:42 +0330

    Created temporary mock song data.

---
+++ lib/src/data/fake_music_data.dart
@@ -0,0 +1,32 @@
+import '../models/song_model.dart';
+
+var fakeSongsList = [
+  Song(
+    title: "Happier than Ever",
+    artist: "Billie Eilish",
+    album: "Happier Than Ever",
+    filePath: "/sdcard/musics",
+    duration: const Duration(minutes: 3),
+  ),
+  Song(
+    title: "Do I Wanna know?",
+    artist: "Arctic Monkeys",
+    album: "AM",
+    filePath: "/sdcard/musics",
+    duration: const Duration(minutes: 3),
+  ),
+  Song(
+    title: "Save Your Tears",
+    artist: "Weeknd",
+    album: "After Hours",
+    filePath: "/sdcard/musics",
+    duration: const Duration(minutes: 3),
+  ),
+  Song(
+    title: "Instant Crush",
+    artist: "Daft Punk",
+    album: "Random Access Memory",
+    filePath: "/sdcard/musics",
+    duration: const Duration(minutes: 3),
+  ),
+];
