a4122d727f845490c204c30c487e8652020083ad
SalehTZ <31685655+SalehTZ@users.noreply.github.com>
2024-01-10 18:00:29 +0330

    Implemented artist image placeholder.

---
+++ lib/src/widgets/common/artist_image/artist_image_placeholder.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:telegram_music_player/src/constants/assets_manager.dart';
+
+import '../../../constants/values_manager.dart';
+
+class ArtistImagePlaceholder extends StatelessWidget {
+  final Uint8List? imageData;
+  const ArtistImagePlaceholder({
+    super.key,
+    required this.imageData,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: const BorderRadius.all(AppRadius.rad999),
+      child: imageData == null
+          ? Image.asset(
+              AssetsManager.defaultSongThumbnail,
+              width: AppSize.s84,
+              height: AppSize.s84,
+            )
+          : Image.memory(
+              imageData!,
+              width: AppSize.s84,
+              height: AppSize.s84,
+            ),
+    );
+  }
+}
