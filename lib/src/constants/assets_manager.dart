52c44e4a8a52b24d99528442ca27f67c528f4a2b
SalehTZ <31685655+SalehTZ@users.noreply.github.com>
2024-01-10 17:58:37 +0330

    Added default thumbnail asset.

---
+++ lib/src/constants/assets_manager.dart
@@ -0,0 +1,15 @@
+const String _assetsFolder = "assets";
+const String _imagesFolder = "$_assetsFolder/images";
+const String _iconsFolder = "$_assetsFolder/icons";
+const String _audioFolder = "$_assetsFolder/audio";
+
+class AssetsManager {
+  static const hamburgerMenuIcon = "$_iconsFolder/hamburger_menu.svg";
+  static const dotsMenuIcon = "$_iconsFolder/menu_dots.svg";
+  static const arrowBackIcon = "$_iconsFolder/arrow_chevron_left.svg";
+  static const searchIcon = "$_iconsFolder/search_icon.svg";
+
+  static const tempSong = "$_audioFolder/R U Mine - Arctic Monkeys.mp3";
+
+  static const defaultSongThumbnail = "$_imagesFolder/default_thumbnail.png";
+}
